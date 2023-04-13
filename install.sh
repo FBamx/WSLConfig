d2c9d0fdf71995f629199fff6c48d0691a217501
root <root@pc>
2023-04-13 15:29:29 +0800

    add

--- install.sh
+++ install.sh
@@ -59,9 +59,11 @@ mv proxy.sh ~/
 
 
 echo "install plugins for zsh"
+cd ~/.oh-my-zsh/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins
 source ~/.zshrc
+cd ~
 echo "--------------------"
 
 
