3febc91a028d64239e4a8305f5114d1830cd9459
root <root@pc>
2023-04-13 14:45:22 +0800

    delete

--- install.sh
+++ install.sh
@@ -63,12 +63,6 @@ source ~/.zshrc
 echo "--------------------"
 
 
-echo "git clone WSLConfig"
-git clone https://github.com/FBamx/WSLConfig.git
-mv WSLConfig/* WSLConfig/.zshrc WSLConfig/.tmux.conf ./
-echo "--------------------"
-
-
 echo "install lazygit"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
