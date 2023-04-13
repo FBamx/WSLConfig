c986fc01a338639c2895c2f7807b9c54cc67d8dc
root <root@pc>
2023-04-13 13:03:24 +0800

    add instlal docker

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # for the new environment
 
 echo "apt install ..."
+sudo apt update
 sudo apt install net-tools
 sudo apt install openssh-server
 sudo apt install gcc
@@ -48,6 +49,23 @@ echo "--------------------"
 
 echo "git clone nvim conf"
 git clone https://github.com/FBamx/lazynvim.git -b master ~/.config/nvim
+echo "--------------------"
+
+
+echo "install docker"
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo "--------------------"
 
 
