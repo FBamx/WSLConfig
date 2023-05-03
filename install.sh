fc7ffb46891df140acfe9e0904c043d6ecc8ddc8
root <root@DESKTOP-0T01DAB>
2023-05-03 23:23:10 +0800

    add installation about go

--- install.sh
+++ install.sh
@@ -53,6 +53,12 @@ cp .tmux.conf ~/
 cp proxy.sh ~/
 cp jandedobbeleer.omp.json ~/
 
+echo "install go"
+wget https://storage.googleapis.com/golang/getgo/installer_linux
+chmod +x ./installer_linux
+./installer_linux
+echo "--------------------"
+
 echo "install lazygit"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
